e9aa62e8382650f4d207f1a2739afcdfc9ffed57
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-26 11:12:19 -0400

    room can be painted

--- lib/room.rb
+++ lib/room.rb
@@ -4,9 +4,18 @@ class Room
     @category = category
     @length = length
     @width = width
+    @has_paint = false
   end
 
   def area
     @length * @width.to_i
+  end
+
+  def is_painted?
+    @has_paint
+  end
+
+  def paint
+    @has_paint = true
   end
 end
